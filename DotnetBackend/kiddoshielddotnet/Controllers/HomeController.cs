3a8a16425344b60c701e88adfd6c31c34238f8c7
Pranavmane7796 <pranavmane3215@gmail.com>
2024-03-15 17:35:57 +0530

    program.cs

--- DotnetBackend/kiddoshielddotnet/Controllers/HomeController.cs
+++ DotnetBackend/kiddoshielddotnet/Controllers/HomeController.cs
@@ -11,15 +11,15 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+    // public IActionResult Index()
+    // {
+    //     return View();
+    // }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+    // public IActionResult Privacy()
+    // {
+    //     return View();
+    // }
 
     // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     // public IActionResult Error()
